aef00f63804ba07795a363121a49561e52e2db89
dougflip <douglasdifilippo@gmail.com>
2015-09-14 10:13:25 -0400

    Quick fix to support a "test mode" in commander
    This isn't very elegant, but it will get me by until I can give it some more thought.
    My main goal is just to get Node Remote back to stable in general.
    So I don't want to add a ton of features until I am happy with what I have again.

--- src/core/commander.js
+++ src/core/commander.js
@@ -3,6 +3,9 @@ var exec = require('child_process').exec;
 
 class Commander {
   exec(cmd){
+    if(!!process.env.NRA_TEST_MODE)
+      return console.log('Command received in TEST MODE', cmd);
+
     console.log("Sending command:", cmd);
     return exec(cmd, (error, stdout) => sys.puts(stdout));
   }
