333f0114873e8b3688747a991707a836211b206d
dougflip <douglasdifilippo@gmail.com>
2015-11-12 18:58:04 -0500

    Removes partials because they don't add anything

--- src/core/browser/commands.js
+++ src/core/browser/commands.js
@@ -1,19 +1,16 @@
 const defaultCommander = require('../commander');
 const keyCommands = require('../keyboard/commands');
-const R = require('ramda');
 
-const browserCommands = function(commander = defaultCommander, keyboard = keyCommands()) {
+module.exports = (commander = defaultCommander, keyboard = keyCommands()) => {
     return {
         launch: url => commander.exec(`firefox ${url}`),
         search: term => commander.exec(`firefox -search "${term}"`),
-        focusAddressBar: R.partial(keyboard.sendKeys, ['ctrl+l']),
-        close: R.partial(keyboard.sendKeys, ['alt+F4']),
-        closeTab: R.partial(keyboard.sendKeys, ['ctrl+F4']),
-        nextTab: R.partial(keyboard.sendKeys, ['ctrl+Tab']),
-        zoomIn: R.partial(keyboard.sendKeys, ['ctrl+Equal']),
-        zoomOut: R.partial(keyboard.sendKeys, ['ctrl+Minus']),
-        actualSize: R.partial(keyboard.sendKeys, ['ctrl+0'])
+        focusAddressBar: () => keyboard.sendKeys('ctrl+l'),
+        close: () => keyboard.sendKeys('alt+F4'),
+        closeTab: () => keyboard.sendKeys('ctrl+F4'),
+        nextTab: () => keyboard.sendKeys('ctrl+Tab'),
+        zoomIn: () => keyboard.sendKeys('ctrl+Equal'),
+        zoomOut: () => keyboard.sendKeys('ctrl+Minus'),
+        actualSize: () => keyboard.sendKeys('ctrl+0')
     }
 }
-
-module.exports = browserCommands
