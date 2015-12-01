7936da1203fc6e146bdc4990f8f803297d4273bc
dougflip <douglasdifilippo@gmail.com>
2015-11-30 22:31:11 -0500

    Adds an index file for consumers
    This allows the server to be "required" and started.
    I'm actually not sure if I will need this or not, but going
    down this route for now.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-const Hapi = require('hapi');
-const core = require('./core');
-const pluginLoader = require('./plugin-loader');
+const app = require('./');
 
-var server = new Hapi.Server(9001, { cors: true });
-
-const coreApi = core.configure(server);
-pluginLoader.configure(coreApi);
-
-server.start(()=> console.log(`server started and running at ${server.info.uri}`));
+app.start();
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const Hapi = require('hapi');
+const core = require('./core');
+const pluginLoader = require('./plugin-loader');
+
+var server = new Hapi.Server(9001, { cors: true });
+
+const coreApi = core.configure(server);
+pluginLoader.configure(coreApi);
+
+module.exports = {
+  start() {
+    server.start(()=> console.log(`server started and running at ${server.info.uri}`));
+  }
+};
