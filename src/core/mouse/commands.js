860b26a2fe26d594368b69e533cb7c0529dd8102
dougflip <douglasdifilippo@gmail.com>
2016-08-22 22:50:59 -0400

    Fix mouse right click
    Not sure why I had this as click 2 before. It should be click 3
    for a right click.

--- src/core/mouse/commands.js
+++ src/core/mouse/commands.js
@@ -5,7 +5,7 @@ module.exports = (commander = defaultCommander) => {
     clickAtPolarAndRestore: (x,y) => commander.exec(`xdotool mousemove --polar ${x} ${y} click 1 mousemove restore`),
     moveRelative: (x,y) => commander.exec(`xdotool mousemove_relative -- ${x} ${y}`),
     leftClick: () => commander.exec("xdotool click 1"),
-    rightClick: () => commander.exec("xdotool click 2"),
+    rightClick: () => commander.exec("xdotool click 3"),
     doubleClick: () => commander.exec("xdotool click --repeat 2 1")
   }
 };
